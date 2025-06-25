1a9711055a92a08ff8d129f40c14c2f6ad28b215
Tyson Lawrie <tlawrie@users.noreply.github.com>
2025-06-26 08:42:37 +1000

    fix: docs link

--- app/routes/flow._index.tsx
+++ app/routes/flow._index.tsx
@@ -27,7 +27,7 @@ export default function About() {
               <div className="flex gap-4">
                 <Link
                   className="inline-flex items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  to="/docs/boomerang-flow/introduction/getting-started"
+                  to="/docs"
                 >
                   Get Started
                 </Link>
